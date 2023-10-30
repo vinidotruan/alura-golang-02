3f4026cabff018fdb0b92302a7c16f59b936e13f
Ruan Vinícius <contatoruanvinicius@gmail.com>
2023-10-30 16:23:17 -0300

    adiciona método de saque

--- main.go
+++ main.go
@@ -9,6 +9,27 @@ type BankAccount struct {
 	balance       float64
 }
 
+func (account *BankAccount) withdraw(value float64) string {
+	canDraw := value > 0 && value <= account.balance
+
+	if canDraw {
+		account.balance -= value
+		return "success"
+	}
+	return "insufficient funds"
+}
+
+func (account *BankAccount) deposit(value float64) (string, float64) {
+	canDeposit := value > 0
+
+	if canDeposit {
+		account.balance += value
+		return "success", account.balance
+	}
+	return "invalid value", account.balance
+
+}
+
 func main() {
 	bankAccount := BankAccount{
 		"Ruan",
@@ -17,5 +38,7 @@ func main() {
 		200.00,
 	}
 
-	fmt.Println(bankAccount)
+	fmt.Println(bankAccount.withdraw(100))
+	fmt.Println(bankAccount.balance)
+	fmt.Println(bankAccount.deposit(100))
 }
